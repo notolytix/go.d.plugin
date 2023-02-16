7a386c7d1a755551ead438d080bf4bc9e589bf75
Apostol Terziev <apostol.terziev@notolytix.com>
2023-02-16 11:00:25 +0200

    Add tests

--- modules/hbase/hbase_test.go
+++ modules/hbase/hbase_test.go
@@ -3,7 +3,10 @@ package hbase
 import (
 	"github.com/netdata/go.d.plugin/agent/module"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -23,4 +26,43 @@ func TestHDFS_Init(t *testing.T) {
 	job := New()
 
 	assert.True(t, job.Init())
+}
+
+func TestHDFS_InitErrorOnCreatingClientWrongTLSCA(t *testing.T) {
+	job := New()
+	job.Client.TLSConfig.TLSCA = "testdata/tls"
+
+	assert.False(t, job.Init())
+}
+
+func TestHBase_Check(t *testing.T) {
+	ts := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(testMasterNodeData)
+			}))
+	defer ts.Close()
+
+	job := New()
+	job.URL = ts.URL
+	require.True(t, job.Init())
+
+	assert.True(t, job.Check())
+	assert.NotZero(t, job.nodeType)
+}
+
+func TestHBase_CheckMasterNode(t *testing.T) {
+	ts := httptest.NewServer(
+		http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				_, _ = w.Write(testMasterNodeData)
+			}))
+	defer ts.Close()
+
+	job := New()
+	job.URL = ts.URL
+	require.True(t, job.Init())
+
+	assert.True(t, job.Check())
+	assert.Equal(t, masterType, job.nodeType)
 }
